716c6d2274cb26e3603936d3e0bf0ac67bc3f88f
jackalQi <32143278+jackalQi@users.noreply.github.com>
2018-07-24 12:00:13 -0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PracticeComponent } from './practice/practice.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PracticeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
